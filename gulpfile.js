3db337367316d7b785216255871d91ba0bfeed38
Robert Watt <hundo.watt@gmail.com>
2018-02-22 18:27:05 -0600

    Update gulp processes
    - gulp default - runs gulp develop which starts browser sync and a watch
    - gulp build - changes index.html to a php file and deletes index.html from dist; this is used when you are ready to upload your lander

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var browser = require('browser-sync').create();
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
 var imagemin = require('gulp-imagemin');
 var clean = require('gulp-clean');
+var ext = require('gulp-ext-replace');
+var path = require('path');
 
 gulp.task('clean', ['clean:index', 'clean:styles', 'clean:kowboykit', 'clean:images']);
 
@@ -19,17 +22,19 @@ gulp.task('clean:scripts', function() {
         .pipe(clean());
 })
 gulp.task('clean:kk', function() {
-    return gulp.src('./dist/**/*.php')
+    return gulp.src(['./dist/exit.php', './dist/thank-you.php', './dist/kowboykit/**/*.php'])
         .pipe(clean());
 })
 gulp.task('styles', ['clean:styles'], function() {
     gulp.src(['./src/styles.scss'])
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browser.stream());
 })
 gulp.task('scripts', ['clean:scripts'], function() {
     gulp.src(['./src/main.js'])
         .pipe(gulp.dest('./dist/scripts'))
+        .pipe(browser.stream());
 })
 gulp.task('images', ['clean:images'], function() {
     gulp.src('./src/assets/images/**/*')
@@ -44,12 +49,20 @@ gulp.task('images', ['clean:images'], function() {
                 ]
             })
         ]))
-        .pipe(gulp.dest('./dist/assets/images'));
+        .pipe(gulp.dest('./dist/assets/images'))
+        .pipe(browser.stream());
 })
 
-gulp.task('kk', ['clean:kk', 'kk-root'], function() {
-    gulp.src(['./src/kowboykit/**/*.php'])
-        .pipe(gulp.dest('./dist/kowboykit'));
+gulp.task('kk', ['clean:kk', 'kk-root', 'kk-api', 'kk-includes'])
+
+gulp.task('kk-api', function() {
+    gulp.src('./src/kowboykit/api/*')
+        .pipe(gulp.dest('./dist/kowboykit/api'));
+})
+
+gulp.task('kk-includes', function() {
+    gulp.src('./src/kowboykit/includes/*')
+        .pipe(gulp.dest('./dist/kowboykit/includes'));
 })
 
 gulp.task('kk-root', function() {
@@ -57,14 +70,36 @@ gulp.task('kk-root', function() {
         .pipe(gulp.dest('./dist'));
 })
 
-gulp.task('default', ['scripts', 'styles', 'images', 'kk'], function() {
-    gulp.src('./src/index.php')
+gulp.task('build', ['clean:index', 'scripts', 'styles', 'images', 'kk'], function() {
+    gulp.src('./src/index.html')
+        .pipe(ext('.php'))
         .pipe(gulp.dest('./dist'));
 })
 
-gulp.task('watch', ['default'], function() {
+gulp.task('clean:index', function() {
+    gulp.src('./dist/index.html')
+        .pipe(clean())
+})
+
+gulp.task('develop', ['index', 'scripts', 'styles', 'images', 'kk'], function() {
+    browser.init({
+        server: "./dist/"
+    })
     gulp.watch('./src/*.scss', ['styles']);
     gulp.watch('./src/*.js', ['scripts']);
     gulp.watch('./src/assets/**/*', ['images']);
-
+    gulp.watch('./src/index.html', ['index']).on('change', browser.reload)
 })
+
+gulp.task('index', function() {
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('serve', function() {
+    browser.init({
+        server: "./dist/"
+    })
+})
+
+gulp.task('default', ['develop']);

45832943513bb5b3ee942c3859c717f0769ab36e
Robert Watt <hundo.watt@gmail.com>
2018-03-16 18:54:59 -0500

    Latest updates

--- src/main.js
+++ src/main.js
@@ -5,18 +5,32 @@ var logo = $('#logo');
 var secondaryText = $('#secondaryText');
 
 $(window).on('load', function() {
-    window.scrollTo(0,0);
-})
+    window.scrollTo(0, 0);
+});
 
-$(window).bind('scroll', function(event) {
+
+// $(window).bind('scroll', function(event) {
+//     var diff = document.documentElement.offsetHeight - window.innerHeight;
+//     var percentage = ((document.documentElement.scrollTop || document.body.scrollTop) / diff);
+//     imageBackground.css('filter', 'blur(' + (percentage * 15) + 'px)');
+//     secondaryText.css('opacity', (percentage > 0.6 ? 1 : 0));
+
+//     if (1 - percentage > 0.4 && percentage > 0) {
+//         logo.css('transform', 'scale(' + ((1 - percentage)) + ',' + ((1 - percentage)) + ') translateX(' + (percentage * 450) + 'px)');
+//     }
+
+// })
+var lastCalc;
+setInterval(function() {
     var diff = document.documentElement.offsetHeight - window.innerHeight;
-    var percentage = ((document.documentElement.scrollTop || document.body.scrollTop) / diff);
-
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    var percentage = scroll / diff;
+    if (lastCalc === percentage) return;
+    lastCalc = percentage;
     imageBackground.css('filter', 'blur(' + (percentage * 15) + 'px)');
-    secondaryText.css('opacity', (percentage > 0.6 ? 1 : 0));
-    
-    if (1 - percentage > 0.4 && percentage > 0) {
-        logo.css('transform', 'scale(' + ((1 - percentage)) + ',' + ((1 - percentage)) + ') translateX(' + (percentage * 450) + 'px)');
-    }
+    secondaryText.css('opacity', (scroll > 160 ? 1 : 0));
 
-})
+    if (1 - percentage > 0.4 && percentage > 0) {
+        logo.css('transform', 'scale(' + ((1 - percentage)) + ',' + ((1 - percentage)) + ') translateY(-' + (percentage * 100) + 'px)');
+    }
+}, 10);

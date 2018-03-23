5830faad539eb3d09899e4e6ea1233f868c34906
Robert Watt <hundo.watt@gmail.com>
2018-03-22 23:47:19 -0500

    Revert "rewrite"
    This reverts commit 22986ff593119b36987816de9f991e4c6c849e49.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-var imageBackground = $('.fixed-image');
-var window = $(window);
-
-$(window).on('load', function() {
-    window.scrollTo(0, 0);
-});
-
-var lastCalc;
-setInterval(function() {
-    var diff = document.documentElement.offsetHeight - window.innerHeight;
-    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    var blurAmount = Math.round(scroll / diff * 12);
-    if (lastCalc === blurAmount) return;
-    lastCalc = blurAmount;
-    imageBackground.css('filter', 'blur(' + blurAmount + 'px)');
-}, 15);
+console.log('Main Javascript Works!')
